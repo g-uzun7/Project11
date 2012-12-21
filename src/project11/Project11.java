44001ed5f2c5891046b32b96b497b16278de3abc
Ali Mert <gforce357@gmail.com>
2012-12-21 15:19:44 +0200

    Project11
    Term Project

---
+++ src/project11/Project11.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bipleash
+ */
+public class Project11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n;
+        int sayi1,sayi2,sayi3,sayi4;
+        Scanner myScanner=new Scanner(System.in);
+        
+  
+        do{
+            System.out.println("Math Problem Solver \n 1.Find the slope of a line from points \n 2.Check your points co-linear or not\n3.Find the slope of a line given by equation");
+            System.out.println("4.Find the slope of a line perpendicular to your line");
+            System.out.println("5.Test your points triangularity");
+            //EDIT HERE
+            System.out.println("Make your choice:");
+            n=myScanner.nextInt();
+            switch(n){
+                case 1:
+                    //slope from points
+                    System.out.println("Enter points like this : x1,x2,y1,y2");
+                    sayi1=myScanner.nextInt();
+                    sayi2=myScanner.nextInt();
+                    sayi3=myScanner.nextInt();
+                    sayi4=myScanner.nextInt();
+                    Slope mypoints=new Slope(sayi1, sayi2, sayi3, sayi4);
+                    mypoints.SlopePoints();
+                    break;
+                case 2:
+                    //co-linear points
+                    break;
+                    
+                case 3:
+                    //slope of line 
+                    break;
+                    
+                case 4:
+                    //slope of perpendicular line
+                    break;
+                case 5:
+                    //triangle
+                    break;
+                case 6:
+                    //exit
+                    break;
+                default:
+                    break;
+            }
+            
+        
+        }while(n!=6);
+    }
+}
---
+++ src/project11/SlopeEq.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project11;
+
+import com.sun.j3d.utils.behaviors.interpolators.KBCubicSplineCurve;
+import java.util.Scanner;
+import sun.security.util.Length;
+
+/**
+ *
+ * @author bipleash
+ */
+public class SlopeEq {
+    
+    String _formula;
+    //char str;
+    Scanner moiScanner=new Scanner(_formula);
+    char[1] obo;
+    int mylength = _formula.length();
+    for(int k=0;k<mylength;k++){
+        moiScanner
+        if(moiScanner.hasNext('x')){
+        }
+
+
+}
+    
+}
